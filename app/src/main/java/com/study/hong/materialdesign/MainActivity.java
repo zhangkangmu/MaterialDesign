e3b43e626054f68545f061f96ae5d5affd6fc672
zhangyuhong <289590351@qq.com>
2019-06-12 01:03:06 +0800

    DrawerLayout的使用，注意只能从左侧边缘开始滑，在代码中设置了点击的事件出现侧滑的功能

--- app/src/main/java/com/study/hong/materialdesign/MainActivity.java
+++ app/src/main/java/com/study/hong/materialdesign/MainActivity.java
@@ -1,5 +1,8 @@
 package com.study.hong.materialdesign;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -10,12 +13,21 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawer_layout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        drawer_layout = findViewById(R.id.drawer_layout);
         setSupportActionBar(toolbar);
+        //这里是getSupportActionBar，不是getActionBar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.img_menu);
+        }
     }
 
     @Override
@@ -36,6 +48,10 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.Settings:
                 Toast.makeText(this,"Settings",Toast.LENGTH_SHORT).show();
+                break;
+            case android.R.id.home:
+                drawer_layout.openDrawer(GravityCompat.START);
+                Toast.makeText(this,"home",Toast.LENGTH_SHORT).show();
                 break;
             default:
                 break;
