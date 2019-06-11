ede555ccacd80c09a59cb2a1d09b78cb83827ab7
zhangyuhong <289590351@qq.com>
2019-06-12 00:23:46 +0800

    Toolbar的使用方式,涉及到主题方面的

--- app/src/main/java/com/study/hong/materialdesign/MainActivity.java
+++ app/src/main/java/com/study/hong/materialdesign/MainActivity.java
@@ -2,6 +2,11 @@ package com.study.hong.materialdesign;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.back:
+                Toast.makeText(this,"back",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(this,"delete",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.Settings:
+                Toast.makeText(this,"Settings",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
